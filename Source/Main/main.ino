6b744e251d7158180fc78cd2bce7c02b8f9b2378
sunio <sunio00000@naver.com>
2020-07-28 07:45:35 +0900

    source code

--- Source/Main/main.ino
+++ Source/Main/main.ino
@@ -1,3 +1,22 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include <Firebase.h>
+#include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
+#include <FirebaseError.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseObject.h>
+
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <DHT_U.h>
@@ -5,6 +24,14 @@
 
 #define DHTPIN 2 
 #define DHTTYPE DHT22 
+
+// Firebase
+#define FIREBASE_HOST "arduino-th-firebase.firebaseio.com/"
+#define FIREBASE_AUTH "LDhCaZIEG94a8il5N4JOGjsjxEcQDXxMC7S6P5vE"
+
+// WIFI
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
  
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -20,6 +47,21 @@ void setup()
   Serial.begin(9600);
   Serial.println("DHT22 Sensor"); 
   Serial.println("I2C Scanner");
+  // WIFI
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting");
+  while(WiFi.status() != WL_Connected){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected.");
+  Serial.print(WiFi.localIP());
+
+  //Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  delay(500);
+  
 }
  
 void loop() {
@@ -34,8 +76,12 @@ void loop() {
     hValue = String(h,2); tValue = String(t,2);
     lcd.setCursor(0,0);
     lcd.print(humidity + hValue + "%");
+    Firebase.setString("humidity/huValue",hValue);
+    delay(300);
     lcd.setCursor(0,1);
     lcd.print(temperature + tValue + "'C");
+    Firebase.setString("temperature/teValue",tValue);
+    delay(300);
     lcd.setCursor(7,2);
     lcd.print("HI. I'm Sunio");
     lcd.setCursor(3,3);
