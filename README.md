350bfcbcb21f7934b974afdadacf466650cf844a
sunio <sunio00000@naver.com>
2020-07-30 00:57:16 +0900

    보드 변경 후, 와이파이를 통해 파이어베이스로 연결 성공

--- README.md
+++ README.md
@@ -6,17 +6,20 @@
  > [200717] Arduino Pro micro 가 DHT 랑 안맞는지 작동을 안함  
  > [200719] 하나더 구매 했는데 이것도 안됨, 이젠 안되는건지 못하는건지 모르겟음  
  > [200722] 빵판이 너무 작아서 쓰기가 어려움 - 배터리, WIFI모듈이 들어갈 자리가 없음  
- > [200728] 납땜기 구하기, 참고 자료 (https://www.youtube.com/watch?v=XeQXK0BkjIo)
+ > [200728] 납땜기 구하기, 참고 자료 (https://www.youtube.com/watch?v=XeQXK0BkjIo)  
+ > [200729] 아두이노 보드 변경 (-> WeMos D1 R2, WIFI 모듈을 이용할 경우 low-level 접근이 요구됨)
 
 --------------------------------------------------------    
 ## 0. Enviroment
 ### SOFTWARE
 - Window10-Arduino 64x
-- VSC, Arduino IDE 
-- [DHT-sensor](https://github.com/adafruit/DHT-sensor-library)         
-- [Adafruit-sensor](https://github.com/adafruit/Adafruit_Sensor)
-- [LiquidCrystal](https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library)
-- [Firebase-ArudinoESP8266](https://github.com/FirebaseExtended/firebase-arduino)
+- VSC, Arduino IDE  
+- <b>사용된 라이브러리</b>  
+  - [DHT-sensor](https://github.com/adafruit/DHT-sensor-library)         
+  - [Adafruit-sensor](https://github.com/adafruit/Adafruit_Sensor)
+  - [LiquidCrystal](https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library)
+  - [Firebase-ArudinoESP8266](https://github.com/FirebaseExtended/firebase-arduino)
+  - [ESP8266 & Driver](http://blog.daum.net/rockjjy99/2453)
 ### HARDWARE
 - (2. 구매 목록 참고)
 - <img src="https://user-images.githubusercontent.com/26760693/88066284-8c7a7000-cba8-11ea-86d3-01b5af752ef0.jpg"  width="300" height="150">  
@@ -29,11 +32,11 @@
 ## 1. Have To Do
 1. ~~hardware로 사용할 아두이노 보드 및 센서, 빵판 등등~~  
 2. ~~온습도 센서 보드로 제어 & Serial output 확인~~  
-3. ~~LCD 기판에 값(temperature, humidity, and so on) 뿌리기~~
-- <img src="https://user-images.githubusercontent.com/26760693/88080350-e9caed00-cbb9-11ea-97f4-aa4d692da0cf.jpg"  width="500" height="250">  
+3. ~~LCD 기판에 값(temperature, humidity, and so on) 뿌리기~~  
+ <img src="https://user-images.githubusercontent.com/26760693/88080350-e9caed00-cbb9-11ea-97f4-aa4d692da0cf.jpg"  width="500" height="250">  
 4. 3D 모델링 및 HW 구조 고민(보조 전원 장치 및 Frame을 어떻게)
-5. WIFI 모듈 연결하고 NoSQL(Firebase realtime DB)에 접근 및 테스트  
-6. raw data를 firebase server를 DB에 저장  
+5. ~~WIFI 모듈 연결하고 NoSQL(Firebase realtime DB)에 접근 및 테스트~~
+6. ~~raw data를 firebase server를 DB에 저장~~    
 7. 저장된 \*.json 파일을 \*.xml 혹은 \*.csv로 export  
 8. 파일을 excel로 불러와 시계열적 그래프로 표현  
 9. 자동화(이건 좀 모르겠따.)         
@@ -45,11 +48,12 @@
 | 품목 | 목적 | 비고 |
 |---|:---:|---:|
 | Arduino Pro micro ATmega32u4 | MCU 컴퓨터 보드 | ❌ 2개 샀는데 실패야 |
-| Arduino Nano ATmega328P | MCU 컴퓨터 보드 | ✔ |
+| Arduino Nano ATmega328P | MCU 컴퓨터 보드 | ❌ |
+| WeMos D1 R2 (ESP8266 in chip) | MCU | ✔ |
 | 빵판(bread board) | 몸통 역할 | ✔ |
 | 점퍼선, 절연 전선 | 전기를 흐르게 해. | ✔ |
 | 적외선 센서 | 거리를 측정하거나 물체 인지 | ✔ |
-| ESP-8266  | WIFI를 사용할 수 있게 해줘 | ✔ |
+| ESP-8266  | WIFI를 사용할 수 있게 해줘 | ❌ AT-command 조작 요구됨 |
 | DHT-22 | 정밀 온습도 센서  | ✔ |
 | LCD 20x4 | 데이터 간단하게라도 봐야지 | ✔ |
 | | |
--- Source/Main/main.ino
+++ Source/Main/main.ino
@@ -9,6 +9,7 @@
 #include <WiFiClientSecure.h>
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
+//#include <WiFi.h>
 
 #include <Firebase.h>
 #include <FirebaseArduino.h>
@@ -17,7 +18,7 @@
 #include <FirebaseHttpClient.h>
 #include <FirebaseObject.h>
 
-#include <LiquidCrystal_I2C.h>
+//#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <DHT_U.h>
 #include <DHT.h> 
@@ -26,23 +27,23 @@
 #define DHTTYPE DHT22 
 
 // Firebase
-#define FIREBASE_HOST "arduino-th-firebase.firebaseio.com/"
+#define FIREBASE_HOST "arduino-th-firebase.firebaseio.com"
 #define FIREBASE_AUTH "LDhCaZIEG94a8il5N4JOGjsjxEcQDXxMC7S6P5vE"
 
 // WIFI
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "U+Net4D48"
+#define WIFI_PASSWORD "47C0559#5J"
  
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
 String humidity = "Humidity: ";
 String temperature = "Temperat: ";
 String hValue, tValue;
 void setup()
 {
-  lcd.init();
-  lcd.begin(20,4);
-  lcd.backlight();
+  //lcd.init();
+  //lcd.begin(20,4);
+  //lcd.backlight();
   dht.begin();
   Serial.begin(9600);
   Serial.println("DHT22 Sensor"); 
@@ -50,7 +51,7 @@ void setup()
   // WIFI
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting");
-  while(WiFi.status() != WL_Connected){
+  while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(500);
   }
@@ -65,28 +66,31 @@ void setup()
 }
  
 void loop() {
-  dht.read();
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  
-  if (isnan(t) || isnan(h)) {
-    Serial.println("Failed to read from DHT");
-  } 
-  else {
-    hValue = String(h,2); tValue = String(t,2);
-    lcd.setCursor(0,0);
-    lcd.print(humidity + hValue + "%");
-    Firebase.setString("humidity/huValue",hValue);
-    delay(300);
-    lcd.setCursor(0,1);
-    lcd.print(temperature + tValue + "'C");
-    Firebase.setString("temperature/teValue",tValue);
-    delay(300);
-    lcd.setCursor(7,2);
-    lcd.print("HI. I'm Sunio");
-    lcd.setCursor(3,3);
-    lcd.print("Call: 01091512551");
-  }
+  Firebase.setString("humidity/huValue","-1");
+  Firebase.setString("temperature/teValue","-1");
   delay(2000);
-  lcd.clear();
+//  dht.read();
+//  float h = dht.readHumidity();
+//  float t = dht.readTemperature();
+//  
+//  if (isnan(t) || isnan(h)) {
+//    Serial.println("Failed to read from DHT");
+//  } 
+//  else {
+//    hValue = String(h,2); tValue = String(t,2);
+//  //  lcd.setCursor(0,0);
+//  //  lcd.print(humidity + hValue + "%");
+//    Firebase.setString("humidity/huValue",hValue);
+//    delay(300);
+//  //  lcd.setCursor(0,1);
+//  //  lcd.print(temperature + tValue + "'C");
+//    Firebase.setString("temperature/teValue",tValue);
+//    delay(300);
+//  //  lcd.setCursor(7,2);
+//  //  lcd.print("HI. I'm Sunio");
+//  //  lcd.setCursor(3,3);
+//  //  lcd.print("Call: 01091512551");
+//  }
+//  delay(2000);
+//  //lcd.clear();
  }
